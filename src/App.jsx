131a798f5c82bd156f56b78024698f85547f6ab8
Handaru <handaruarioseto12@gmail.com>
2026-09-09 20:16:25 +0700

    fix: case-sensitive import untuk Landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Landing from "./pages/Landing";
+import Landing from "./pages/landing";
 import Birthday from "./pages/Birthday";
 import Quiz from "./pages/Quiz";
 import EighteenThings from "./pages/EighteenThings";
